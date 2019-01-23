f0f2c6a00ccc134959e2016ddee6233609fca5e3
Krishnasis Mandal <krishnasismandal97@gmail.com>
2019-01-23 10:49:26 +0530

    save audio

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ from threading import Thread
 #set properties of audio
 CHUNK = 2**11
 RATE = 44100
-CHANNELS = 2
+CHANNELS = 1
 
 #open stream
 p=pyaudio.PyAudio()
@@ -29,16 +29,30 @@ while(True):
         peak=np.average(np.abs(data))*2
         
         #check if silent
-        if(peak<400):
+        if(peak<400): ##do some tuning here boi
             if(listening):
-                #saveFile
+
+                ##Save the audio to disk
+                filename='test'+str(random.randint(0,10000))+'.wav'
+                wavefile=wave.open(filename,'wb')
+                wavefile.setnchannels(CHANNELS)
+                wavefile.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+                wavefile.setframerate(RATE)
+                wavefile.writeframes(b''.join(frames))
+                wavefile.close
+                frames=[]
                 listening=False
+                print("SAVED")
+
             print("Not listening")
         else:
             listening=True
             print("Listening")
+            data_audio=stream.read(CHUNK)
+            frames.append(data_audio)
+            
 
-            ##Save the audio to disk
+            
             
 
 
